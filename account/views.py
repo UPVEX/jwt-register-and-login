ce7d86300338faaacdd621de0fe40d6b6d91e239
unknown <nimamollayi@gmail.com>
2023-05-03 19:43:54 +0430

    fix some bug

--- account/views.py
+++ account/views.py
@@ -13,23 +13,3 @@ class RegisterViewset(mixins.CreateModelMixin, viewsets.GenericViewSet):
 
 class LoginViewset(mixins.CreateModelMixin, viewsets.GenericViewSet):
     serializer_class = LoginSerializer
-
-    def create(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data)
-        serializer.is_valid(raise_exception=True)
-
-        user = authenticate(
-            username=serializer.validated_data['username'],
-            password=serializer.validated_data['password']
-        )
-
-        if not user:
-            return Response({'error': 'Invalid Credentials'}, status=400)
-        token = RefreshToken.for_user(user)
-        return Response({
-            'user_id': user.id,
-            'username': user.username,
-            'email': user.email,
-            'access_token': str(token.access_token),
-            'refresh_token': str(token)
-        })
